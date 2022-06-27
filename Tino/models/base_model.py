fca28b171edb250b253951dac9663676ef66df10
root <martin.liporace@gmail.com>
2022-06-27 16:09:41 -0700

    base_model.py

--- Tino/models/base_model.py
+++ Tino/models/base_model.py
@@ -3,7 +3,7 @@
 
 import json
 import uuid
-import datetime
+from datetime import datetime
 import time
 
 
@@ -13,19 +13,20 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """init"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        print("id", self.id)
-
-    def Date(self):
-        """return de date and time in isoformat"""
-        Date = datetime.datetime.now()
-        DateTime_in_ISOFormat = Date.isoformat()
-        return(DateTime_in_ISOFormat)
+        if kwargs:
+            dtf = "%Y-%m-%dT%H:%M:%S.%f"
+            for keys, values in kwargs.items():
+                if keys != "__class__":
+                    if keys == "created_at" or keys == "updated_at":
+                        values = datetime.strptime(values, dtf)
+                    setattr(self, keys, values)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """[<class name>] (<self.id>) <self.__dict__>"""
@@ -35,12 +36,12 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at
         with the current datetime"""
-        self.updated_at = str(BaseModel.Date(self))
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance"""
         dic = self.__dict__.copy()
-        dic["created_at"] = self.created_at
-        dic["updated_at"] = self.updated_at
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
         dic["__class__"] = self.__class__.__name__
         return dic
