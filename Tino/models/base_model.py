e6111e3c3e92fe56d87207e4d1d41608b92d0292
root <martin.liporace@gmail.com>
2022-06-27 07:52:36 -0700

    models/

--- Tino/models/base_model.py
+++ Tino/models/base_model.py
@@ -12,20 +12,29 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self, id=None, created_at, updated_at):
+    def __init__(self):
         """init"""
 
-        self.id = id
-        if self.id is None:
-            self.id = str(uuid.uuid4())
-        self.created_at = Date()
-        self.updated_at = Date()
+        self.id = str(uuid.uuid4())
+        self.created_at = str(BaseModel.Date(self))
+        self.updated_at = str(BaseModel.Date(self))
         print("id", self.id)
 
     def Date(self):
         Date = datetime.datetime.now()
         DateTime_in_ISOFormat = Date.isoformat()
-        return(str(DateTime_in_ISOFormat))
+        return(DateTime_in_ISOFormat)
 
     def __str__(self):
-        return("[BaseModel] ({})".format(self.id))
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+    def save(self):
+        self.updated_at = str(BaseModel.Date(self))
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values of the instance"""
+        dic = self.__dict__.copy()
+        dic["created_at"] = self.created_at
+        dic["updated_at"] = self.updated_at
+        dic["__class__"] = self.__class__.__name__
+        return dic
