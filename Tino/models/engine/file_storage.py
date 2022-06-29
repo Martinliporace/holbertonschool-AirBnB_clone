be3beb44c70f8acaa2748a082eb38f9ac7c541d8
root <martin.liporace@gmail.com>
2022-06-29 12:03:48 -0700

    console.py

--- Tino/models/engine/file_storage.py
+++ Tino/models/engine/file_storage.py
@@ -1,37 +1,45 @@
+
 #!/usr/bin/python3
 """FileStorage class"""
-
 import json
 from models.base_model import BaseModel
-from os import path
-
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
-
+    """storage engine
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ returns the dictionary __objects"""
-        return(self.__objects)
+        """Return the dictionary __objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects[(str(obj.__class__.__name__)) + '.' + (str(obj.id))] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        str = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(str, obj.id)] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: self.__file_path)"""
-        aux = {}
-        for keys, values in self.__objects.items():
-            aux[keys] = values.to_dict()
-
-        with open(self.__file_path, 'w') as f:
+        """Serialize __objects to the JSON file __file_path"""
+        odict = FileStorage.__objects
+        aux = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
             json.dump(aux, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """Deserialize the JSON to __objects"""
         try:
-            with open(self.__file_path, 'r') as json_file:
-                obj = json.load(json_file)
+            with open(FileStorage.__file_path) as f:
+                aux = json.load(f)
+                for o in aux.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
         except FileNotFoundError:
-            pass
+            return
