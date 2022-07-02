ee3a19fd978cb4d6835b35d26ab75c1a401867ee
root <martin.liporace@gmail.com>
2022-07-02 06:41:52 -0700

    file storage pcs fxd

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
-
 #!/usr/bin/python3
 """FileStorage class"""
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -9,6 +9,7 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
 
 class FileStorage:
     """storage engine
