463e39b863f39b4aa411b49f09410e865143d5dd
root <martin.liporace@gmail.com>
2022-07-02 06:33:48 -0700

    amenity.py pcs fixed

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,12 @@
 """Amenity class"""
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
     """Represent an amenity
     Attributes:
     name (str) name of the amenity
     """
+
 
 name = ""
