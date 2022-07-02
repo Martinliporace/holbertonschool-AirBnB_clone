494e0f847d9a59f2bd71a5302c8c694d0c05c8a2
root <martin.liporace@gmail.com>
2022-07-02 07:36:46 -0700

    yo que se

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ import json
 import uuid
 from datetime import datetime
 import time
-from models import storage
+import models
 
 class BaseModel:
     """
@@ -27,7 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """[<class name>] (<self.id>) <self.__dict__>"""
@@ -38,7 +38,7 @@ class BaseModel:
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance"""
