bd6c9cc6fc6060ad0de75aaabac7e2325e5b9621
root <martin.liporace@gmail.com>
2022-07-02 07:24:14 -0700

    base_model.py fxd

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """[<class name>] (<self.id>) <self.__dict__>"""
@@ -38,7 +38,7 @@ class BaseModel:
         """updates the public instance attribute updated_at
         with the current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance"""
